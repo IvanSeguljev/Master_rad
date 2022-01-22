1f9a6d6e8bba44eac9f29a6435d0358e92615363
Ivan <ivan.seguljev@yahoo.com>
2022-01-22 22:45:17 +0100

    Hamburger

--- app/src/main/java/com/ivanseguljev/master_rad/MainActivity.java
+++ app/src/main/java/com/ivanseguljev/master_rad/MainActivity.java
@@ -7,11 +7,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton buttonEnchancedVision;
     ImageButton buttonSignDetection;
+    ImageView imageViewHamburger;
+    private boolean isHamburgerOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
 
         buttonEnchancedVision = findViewById(R.id.btn_enchanced_vision);
         buttonSignDetection = findViewById(R.id.btn_sign_detection);
+        imageViewHamburger = findViewById(R.id.imageViewHamburger);
+
+        imageViewHamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isHamburgerOpen){
+                    imageViewHamburger.setImageResource(R.drawable.close_hamburger);
+                    isHamburgerOpen=true;
+                }
+                else{
+                    imageViewHamburger.setImageResource(R.drawable.hamburger);
+                    isHamburgerOpen=false;
+                }
+            }
+        });
+
 
         //sign detection should lead to view that shows cutouts of detected signs
         buttonSignDetection.setOnClickListener(new View.OnClickListener() {
