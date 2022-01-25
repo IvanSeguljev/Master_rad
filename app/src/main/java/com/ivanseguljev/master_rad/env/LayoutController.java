885d37986b089d96d74def9929da0d55e140f8f6
Ivan <ivan.seguljev@yahoo.com>
2022-01-25 20:06:04 +0100

    Layout

--- app/src/main/java/com/ivanseguljev/master_rad/env/LayoutController.java
+++ app/src/main/java/com/ivanseguljev/master_rad/env/LayoutController.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.ivanseguljev.master_rad.EnchancedVision;
+import com.ivanseguljev.master_rad.MainActivity;
 import com.ivanseguljev.master_rad.R;
 
 public class LayoutController {
@@ -22,6 +23,8 @@ public class LayoutController {
     //using layout as a button so it looks nice :)
     LinearLayout buttonRoadsignDetection;
     LinearLayout buttonEnchancedVision;
+    LinearLayout buttonHome;
+    LinearLayout buttonAbout;
     Context context;
 
     public LayoutController init(Context context) {
@@ -30,10 +33,13 @@ public class LayoutController {
         imageViewHamburger = ((Activity) context).findViewById(R.id.imageViewHamburger);
         drawerLayout = ((Activity) context).findViewById(R.id.drawer_layout);
         imageViewDrawerClose = ((Activity) context).findViewById(R.id.imageViewDrawerClose);
+        //navigation buttons
         buttonRoadsignDetection = ((Activity) context).findViewById(R.id.layout_roadsign_detection);
         buttonEnchancedVision = ((Activity) context).findViewById(R.id.layout_computer_vision);
+        buttonHome = ((Activity) context).findViewById(R.id.layout_home);
+        buttonAbout = ((Activity) context).findViewById(R.id.layout_about);
         System.out.println(context.getClass());
-
+        //Hamburger button for opening and closing nav menu
         imageViewHamburger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,31 +52,51 @@ public class LayoutController {
                 drawerLayout.closeDrawer(Gravity.LEFT);
             }
         });
+        //Roadsign detection
         buttonRoadsignDetection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 drawerLayout.closeDrawer(Gravity.LEFT);
                 Toast.makeText(context.getApplicationContext(), "to be implemented", Toast.LENGTH_SHORT).show();
+                //TODO implement this
             }
         });
-        if (!EnchancedVision.class.equals(context.getClass())) {
-            buttonEnchancedVision.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    drawerLayout.closeDrawer(Gravity.LEFT);
+        //Enchanced vision
+        buttonEnchancedVision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                if (!EnchancedVision.class.equals(context.getClass())) {
                     Intent enchancedVisionIntent = new Intent(context, EnchancedVision.class);
                     context.startActivity(enchancedVisionIntent);
-                }
-            });
-        }else {
-            buttonEnchancedVision.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    drawerLayout.closeDrawer(Gravity.LEFT);
+                } else {
                     Toast.makeText(context.getApplicationContext(), "Trenutno na toj aktivnosti", Toast.LENGTH_SHORT).show();
                 }
-            });
-        }
+            }
+        });
+        //Home
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                if (!MainActivity.class.equals(context.getClass())) {
+                    Intent enchancedVisionIntent = new Intent(context, MainActivity.class);
+                    context.startActivity(enchancedVisionIntent);
+                } else {
+                    Toast.makeText(context.getApplicationContext(), "Trenutno na toj aktivnosti", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //About
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                Toast.makeText(context.getApplicationContext(), "to be implemented", Toast.LENGTH_SHORT).show();
+                //TODO implement this
+            }
+        });
+
         return this;
     }
 }
